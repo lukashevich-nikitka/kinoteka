e65ab5701cdcf5549bc5372db2217954be80efed
Nikita <lukashevich-nikitka@bk.ru>
2022-08-02 00:49:15 +0300

    feat: add folder architecture and all necessary packages

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT ?? 4200;
+
+const app = express();
+
+app.use(express.json({ extended: true }));
+
+app.use(
+    cors({
+      origin: "*",
+    })
+  );
+
+app.listen(PORT, () => {
+    console.log(`Server has been started on port ${PORT}`)
+})
